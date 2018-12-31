47ae1bf3801a8c33447baa14f372e7972e78aada
오승빈 <sboh1214@gmail.com>
2019-01-01 02:20:38 +0900

    Sync

--- SnackBar/app/src/main/java/com/iasa/sboh1214/snackbar/MainActivity.kt
+++ SnackBar/app/src/main/java/com/iasa/sboh1214/snackbar/MainActivity.kt
@@ -2,11 +2,27 @@ package com.iasa.sboh1214.snackbar
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    var snackbar:Snackbar?=null
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onPopSnackbarButtonClicked(view: View)
+    {
+        var snackbar:Snackbar = Snackbar.make(view,"Hello World",Snackbar.LENGTH_INDEFINITE)
+        snackbar.show()
+    }
+
+    fun onDismissButtonClicked(view: View)
+    {
+
     }
 }
